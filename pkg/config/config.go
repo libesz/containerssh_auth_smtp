5b4d976cf624ca7a52a80de1e1ffdf77e0e903f9
Gergo Huszty <gergo.huszty@ibm.com>
2021-10-19 16:01:45 +0000

    guest containers have proper names

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,15 +3,31 @@ package config
 import (
 	"net/http"
 
+	"crypto/rand"
+	"unsafe"
+
 	"github.com/containerssh/configuration/v2"
 	"github.com/containerssh/log"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/mount"
 )
 
+var alphabet = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
 const (
-	contentMountPoint = "/content"
+	contentMountPoint           = "/content"
+	containerNamePrefix         = "cssh-client"
+	containerNameRandomTailSize = 5
 )
+
+func generate(size int) string {
+	b := make([]byte, size)
+	rand.Read(b)
+	for i := 0; i < size; i++ {
+		b[i] = alphabet[b[i]/5]
+	}
+	return *(*string)(unsafe.Pointer(&b))
+}
 
 func NewConfigReqHandler(logger log.Logger, mapping MappingFileHandler) (http.Handler, error) {
 	return configuration.NewHandler(&configReqHandler{logger, mapping}, logger)
@@ -30,12 +46,14 @@ func (c configReqHandler) OnConfig(
 		config.Docker.Execution.Launch.ContainerConfig = &container.Config{}
 	}
 
-	config.Docker.Execution.Launch.ContainerConfig.WorkingDir = contentMountPoint
-
 	volumeName, err := c.mapping.GetUserVolumeName(request.Username)
 	if err != nil {
 		return config, err
 	}
+
+	config.Docker.Execution.Launch.ContainerName = containerNamePrefix + "-" + volumeName + "-" + generate(containerNameRandomTailSize)
+
+	config.Docker.Execution.Launch.ContainerConfig.WorkingDir = contentMountPoint
 
 	if config.Docker.Execution.Launch.HostConfig == nil {
 		config.Docker.Execution.Launch.HostConfig = &container.HostConfig{}
@@ -49,5 +67,6 @@ func (c configReqHandler) OnConfig(
 		Target:        contentMountPoint,
 		VolumeOptions: &mount.VolumeOptions{},
 	})
+
 	return config, err
 }
